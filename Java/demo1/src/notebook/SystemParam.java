ddfe0a8f9652b29bacf85ec6adadb09a159fa328
Charles <ywzonly@163.com>
2017-09-11 23:46:08 +0800

    update code

---
+++ Java/demo1/src/notebook/SystemParam.java
@@ -0,0 +1,14 @@
+package notebook;
+
+/** 
+ * 系统参数 
+ * @author daisy 
+ * 
+ */  
+public class SystemParam {  
+      
+    /** 
+     * 当前系统换行符 
+     */  
+    public static final String LINE_SEPARATOR=System.getProperty("line.separator");  
+}  
